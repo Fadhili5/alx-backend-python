e299d54bd71e3a8012458148888ec7fc126d5d7b
fadhili5 <denisfadhili5@gmail.com>
2025-07-29 11:09:07 +0300

    fix: changed the import path for the package drf-nested-routers in urls.py

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from rest_framework import routers as nested_routers
+from rest_framework_nested import routers as nested_routers
 from rest_framework import routers
 from .views import ConversationViewSet, MessageViewSet, UserViewSet
 
