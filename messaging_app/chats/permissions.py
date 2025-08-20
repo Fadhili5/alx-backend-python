48835cd7fa5ca3eb7839c8196c3d0e49d68ce049
fadhili5 <denisfadhili5@gmail.com>
2025-08-20 19:49:42 +0300

    feat: implement permission class to restrict access to conversation participants

---
+++ messaging_app/chats/permissions.py
@@ -0,0 +1,15 @@
+from rest_framework import permissions
+
+class IsParticipantOfConversation(permissions.BasePermission):
+    """
+    Allows access only to participants of conversation.
+    """
+    
+    def has_object_permission(self, request, view, obj):
+        user = request.user
+        if hasattr(obj, 'participants'):
+            return user in obj.participants.all()
+        elif hasattr(obj, 'conversation'):
+            return user in obj.conversation.participants.all()
+        return False
+        
