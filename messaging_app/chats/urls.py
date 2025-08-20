6c887aa243adcced389f972c33f7bddadd2341e7
fadhili5 <denisfadhili5@gmail.com>
2025-08-20 19:10:48 +0300

    feat: add token authentication endpoint using CustomTokenObtainPairView

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -2,6 +2,8 @@ from django.urls import path, include
 from rest_framework_nested import routers as nested_routers
 from rest_framework import routers
 from .views import ConversationViewSet, MessageViewSet, UserViewSet
+from rest_framework_simplejwt.views import TokenObtainPairView
+from .serializers import CustomTokenObtainPairSerializer
 
 router = routers.DefaultRouter()
 router.register(r'conversations', ConversationViewSet, basename='conversations')
@@ -11,7 +13,13 @@ router.register(r'users', UserViewSet, basename='users')
 conversations_router = nested_routers.NestedDefaultRouter(router, r'conversations', lookup='conversation')
 conversations_router.register(r'messages', MessageViewSet, basename='conversation-messages')
 
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
+
+
 urlpatterns = [
     path('', include(router.urls)),
     path('', include(conversations_router.urls)),
+    path('token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
 ]
