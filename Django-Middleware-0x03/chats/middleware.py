c43d881d16cca57f7374419a471409417926fa24
fadhili5 <denisfadhili5@gmail.com>
2025-09-25 01:12:39 +0800

    fix: Added RestrictAccessByTimeMiddleware and OffensiveLanguageMiddleware classes that restrict access to the messaging app during certain hours of the day and limits number of chat a user can send within a certain time window based on their IP address respectively.

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -1,6 +1,7 @@
 from datetime import datetime
+from django.http import HttpResponseForbidden
 
-class RequestLoggingMiddleware
+class RequestLoggingMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
         
@@ -11,3 +12,42 @@ class RequestLoggingMiddleware
             log_file.write(log_entry)
         response = self.get_response(request)
         return response
+    
+class RestrictAccessByTimeMiddleware:
+    def __init__(self, request):
+        current_hour = datetime.now().hour
+        if not(18 <= current_hour < 21):
+            return HttpResponseForbidden("Access to messaging app is restricted outside 9pm and 6pm")
+        return self.get_response(request)
+    
+class OffensiveLanguageMiddleware:
+    message_log = {}
+    
+    def __init__(self, get_response):
+        self.get_response = get_response
+        
+    def __call__(self, request):
+        if request.path.startswith('/api/messages/') and request.method == 'POST':
+            ip = self.get_client_ip(request)
+            now = datetime.now()
+            self.cleanup(ip, now)
+            if ip not in self.message_log:
+                self.message_log[ip] = []
+            self.message_log[ip] = [t for t in self.message_log[ip] if now - t < timedelta(minutes=1)]
+            if len(self.message_log[ip]) >= 5:
+                return HttpResponseForbidden("Message rate limit exceeded: Max 5 messages per minute.")
+            self.message_log[ip].append(now)
+        return self.get_response(request)
+    
+    def get_client_ip(self, request):
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(',')[0]
+        else:
+            ip = request.META.get('REMOTE_ADDR')
+        return ip
+    
+    def cleanup(self, ip, now):
+        #Remove timestamps older than 1 minute
+        if ip in self.message_log:
+            self.message_log[ip] = [t for t in self.message_log[ip] if now - t < timedelta(minutes=1)]
