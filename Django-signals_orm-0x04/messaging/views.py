48d141983902da77d4ebadf906cd8b53f90a938a
fadhili5 <denisfadhili5@gmail.com>
2025-09-27 20:06:11 +0800

    fix: remove unused imports and clean up code in views.py

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import get_object_or_404
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST
 from django.contrib.auth.decorators import login_required
+from .models import Message
 
 User = get_user_model()
 
@@ -13,6 +14,19 @@ def delete_user(request):
     user.delete()
     return JsonResponse({'message': 'User account and related data deleted successfully.'})
 
+@login_required
+def unread_inbox(request):
+    unread_messages = Message.unread.unread_for_user(request.user)
+    data = [
+        {
+            'id': msg.id,
+            'sender': msg.sender.username,
+            'content': msg.content,
+            'timestamp': msg.timestamp
+        }
+        for msg in unread_messages
+    ]
+    return JsonResponse({'unread_messages': data})
 
 
 
