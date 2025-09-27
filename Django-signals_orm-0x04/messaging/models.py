307be1303ea25613d42c42a67a1d6fa3d206514e
fadhili5 <denisfadhili5@gmail.com>
2025-09-27 20:23:10 +0800

    feat: restore UnreadMessagesManager class definition in models.py

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -4,9 +4,9 @@ from .models import UnreadMessagesManager
 
 User = get_user_model()
 
-# class UnreadMessagesManager(models.Model):
-#     def for_user(self, user):
-#         return self.get_queryset().filter(receiver=user, read=False).only('id', 'sender', 'content', 'timestamp')
+class UnreadMessagesManager(models.Model):
+    def for_user(self, user):
+        return self.get_queryset().filter(receiver=user, read=False).only('id', 'sender', 'content', 'timestamp')
 
 class Message(models.Model):
     sender = models.ForeignKey(User, related_name='sent_messages', on_delete=models.CASCADE)
